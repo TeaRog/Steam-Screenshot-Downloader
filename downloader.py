cf5ae8fbbb8fb2475a1845732fb9527c3370454a
Sandaru Fernando <sandarustar7@gmail.com>
2020-12-03 02:22:18 -0700

    Cleaned up a bit

--- downloader.py
+++ downloader.py
@@ -17,7 +17,6 @@ def writeImages(screenshotDataDict):
 
 
 def downloadImage(sharedFileUrl):
-    global screenshotDataDict
     #Open the sharedfiles page
     html = urllib.request.urlopen(sharedFileUrl)
     soup = BeautifulSoup(html, "html.parser")
@@ -33,41 +32,3 @@ def downloadImage(sharedFileUrl):
     #putting into a dict with the steam image id and the image data (its probably not a good idea to put the image data as a hashmap?)
     id = int(parse_qs(urlparse(sharedFileUrl).query)["id"][0])
     return (id, screenshotData)
-
-
-
-"""
-parsed = list(urlparse('https://steamcommunity.com/sharedfiles/filedetails/?id=1481162777'))
-for id in idlist:
-    #print(id)
-    #print(parsed)
-    parsed[4] = id
-    screenshotcontainer = urlunparse(parsed)
-    #print(screenshot)
-    htmlfile = urllib.request.urlopen(screenshotcontainer)
-    soup = BeautifulSoup(htmlfile, 'html.parser')
-    for link in soup.find_all('a'):
-        parsed2 = urlparse(link.get('href'))
-        if parsed2.netloc=='steamuserimages-a.akamaihd.net':
-            try:
-                #print(parsed2)
-                truescreenshoturl=urlunparse(list(parsed2))
-                screenshotresponse = urllib.request.urlopen(truescreenshoturl)
-                screenshotdata = screenshotresponse.read()
-                filename = 'file' +str(iterator)+ '.jpg'
-                myfile = open(filename, 'wb')
-                myfile.write(screenshotdata)
-                myfile.close()
-                iterator = iterator + 1
-                print ('Saved ' + truescreenshoturl)
-            except urllib.error.HTTPError as err:
-                if err.code == 504:
-                    print('504 encountered" ' + truescreenshoturl)
-                    iterator = iterator + 1
-                    continue
-                else:
-                    raise
-"""           
-    
-    
-    
