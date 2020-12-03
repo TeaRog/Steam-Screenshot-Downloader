91851c764f8fde07efb66c683b5f37b761c5fed0
Sandaru Fernando <sandarustar7@gmail.com>
2020-12-03 02:09:38 -0700

    add multiprocessing to download code

--- downloader.py
+++ downloader.py
@@ -1,32 +1,11 @@
 from urllib.parse import urlparse
 from urllib.parse import parse_qs
 import os.path
+from multiprocessing import Manager, Pool
 
 import urllib.request
 from bs4 import BeautifulSoup
 
-
-def downloadImages(sharedFilesUrlList):
-    print("Downloading images")
-    screenshotDataDict = {}
-    for sharedFileUrl in sharedFilesUrlList:
-        #Open the sharedfiles page
-        html = urllib.request.urlopen(sharedFileUrl)
-        soup = BeautifulSoup(html, "html.parser")
-
-        #Find the image and get its source/CDN url.
-        hyperlink = soup.find(id="ActualMedia")
-        CDNUrl = urlparse(hyperlink.get("src"))._replace(query=None).geturl()
-        
-        #Open the source url and read the image data
-        screenshotURL = urllib.request.urlopen(CDNUrl)
-        screenshotData = screenshotURL.read()
-
-        #putting into a dict with the steam image id and the image data (its probably not a good idea to put the image data as a hashmap?)
-        id = int(parse_qs(urlparse(sharedFileUrl).query)["id"][0])
-        screenshotDataDict[id] = screenshotData
-
-    return screenshotDataDict
 
 def writeImages(screenshotDataDict):
     if not (os.path.exists("imgs")):
@@ -35,6 +14,25 @@ def writeImages(screenshotDataDict):
         filename = str(key) + ".jpg"
         with open(os.path.join("imgs", filename), 'wb') as imgFile:
             imgFile.write(value)
+
+
+def downloadImage(sharedFileUrl):
+    global screenshotDataDict
+    #Open the sharedfiles page
+    html = urllib.request.urlopen(sharedFileUrl)
+    soup = BeautifulSoup(html, "html.parser")
+
+    #Find the image and get its source/CDN url.
+    hyperlink = soup.find(id="ActualMedia")
+    CDNUrl = urlparse(hyperlink.get("src"))._replace(query=None).geturl()
+
+    #Open the source url and read the image data
+    screenshotURL = urllib.request.urlopen(CDNUrl)
+    screenshotData = screenshotURL.read()
+
+    #putting into a dict with the steam image id and the image data (its probably not a good idea to put the image data as a hashmap?)
+    id = int(parse_qs(urlparse(sharedFileUrl).query)["id"][0])
+    return (id, screenshotData)
 
 
 
--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 import urlfetcher
 import downloader
+from multiprocessing import Manager, Pool
 
-userId = "combatperson7"
-appId = 0
 
-sharedUrls = urlfetcher.getURLs(userId, appId)
-urlfetcher.dumpURLs(sharedUrls)
-#sharedUrls = ["https://steamcommunity.com/sharedfiles/filedetails/?id=2307100788"]
-downloader.writeImages(downloader.downloadImages(sharedUrls))
+if __name__ == '__main__':
+
+    userId = "combatperson7"
+    appId = 0
+
+    sharedUrls = urlfetcher.getURLs(userId, appId)
+    urlfetcher.dumpURLs(sharedUrls)
+
+    print("Downloading images")
+    pool = Pool(processes=6)
+    results = pool.map(downloader.downloadImage, sharedUrls)
+    pool.close()
+    pool.join()
+
+    screenshotDataDict = {}
+    for id, img in results:
+        screenshotDataDict[id] = img
+
+
+    downloader.writeImages(screenshotDataDict)
+
+
+
+
+
+
+
+
