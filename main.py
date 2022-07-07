e0cdee653c285fb88f80d47e90d359bb4125c216
TeaRog <tyler.s.w.rogers@gmail.com>
2022-07-06 18:35:56 -0700

    updated to get all pages

--- main.py
+++ main.py
@@ -2,32 +2,40 @@ import urlfetcher
 import downloader
 from multiprocessing import Manager, Pool
 
-
-if __name__ == '__main__':
-
-    userId = "combatperson7"
-    appId = 0
-
-    sharedUrls = urlfetcher.getURLs(userId, appId)
+def getImages(userId, appId, pageNum):
+    sharedUrls = urlfetcher.getURLs(userId, appId, pageNum)
     urlfetcher.dumpURLs(sharedUrls)
-
-    print("Downloading images")
     pool = Pool(processes=6)
     results = pool.map(downloader.downloadImage, sharedUrls)
     pool.close()
     pool.join()
 
+    count = 0
     screenshotDataDict = {}
+    
     for id, img in results:
         screenshotDataDict[id] = img
-
+        count += 1
 
     downloader.writeImages(screenshotDataDict)
 
+    return count
 
+if __name__ == '__main__':
 
+    userId = "CHANGE TO YOUR PERSONAL STEAM ACCOUNT ID"
+    appId = 0 # specific game to download, 0 = all games
+    
+    page = 1 # leave unchanged
 
+    print("Starting Steamcloud Screenshot Download: Account: "+str(userId))
 
+    while True:# Do While Loop Emulation
+        print("  Downloading Page "+str(page))
 
-
-
+        count = getImages(userId, appId, page)
+        print("    "+str(count) + " Images downloaded and saved to imgs folder")
+        if count > 0:
+            page+=1
+        else:
+            break
--- urlfetcher.py
+++ urlfetcher.py
@@ -2,28 +2,25 @@ from bs4 import BeautifulSoup
 import urllib.request
 from urllib.parse import urlparse
 
-def getURLs(userId, appId):
-
+def getURLs(userId, appId, page):
+    print("    Searching for Screenshots Batch...")
     sharedFilesList = []
     newAdded = True
-    currPage = 1
-    print("Searching for screenshots")
+    
     while(newAdded):
         newAdded = False
-        urlString = "https://steamcommunity.com/id/" + userId + "/screenshots/?p=" + str(currPage) + "&appid=" + str(appId) + "&sort=newestfirst&browsefilter=myfiles&view=imagewall"
-        
+        urlString = "https://steamcommunity.com/profiles/"+str(userId)+"/screenshots/?p="+str(page)+"&appid="+str(appId)+"&sort=oldestfirst&browsefilter=myfiles&view=grid"
         url = urllib.request.urlopen(urlString)
         soup = BeautifulSoup(url, "html.parser")
+
         for hyperlink in soup.findAll('a'):
             parse = urlparse(hyperlink.get("href"))
             if (parse.path == "/sharedfiles/filedetails/" and hyperlink.get("href") not in sharedFilesList):
                 newAdded = True
                 sharedFilesList.append(hyperlink.get("href"))
-        currPage = currPage + 1
+
     return sharedFilesList
-    
 
 def dumpURLs(sharedFilesList):
-    print ("Dumping URLs to file")
     with open ('idlist.txt', 'w') as fp:
         fp.writelines("%s\n" % item for item in sharedFilesList)
